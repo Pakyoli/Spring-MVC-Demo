702fc8a3febf9ededa64f7f328912b35448b79f6
Patrick <pakyo_li@hotmail.com>
2016-06-11 01:10:38 -0400

    update

--- src/main/java/com/patspringframework/dao/UserDAOImpl.java
+++ src/main/java/com/patspringframework/dao/UserDAOImpl.java
@@ -1,11 +1,10 @@
 package com.patspringframework.dao;
 
-import com.patspringframework.Utilities.dbUtil;
 import com.patspringframework.dto.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.stereotype.Repository;
 
+import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -17,46 +16,52 @@ import java.util.List;
  * Created by pakyo_000 on 6/6/2016.
  */
 @Repository
-public class UserDAOImpl implements UserDAO {
+public class UserDAOImpl implements DAO {
 
     @Autowired
-    DriverManagerDataSource dataSource;
+    DataSource dataSource;
 
     @Override
     public void create(Object object) {
 
+        Connection conn = null;
         PreparedStatement stmt = null;
         User user = (User)object;
 
         try{
-            String query = "insert into users (first_name, last_name, username, password, enabled, role_id, created_date) values (?,?,?,?,?,?,?)";
-            stmt = dbUtil.getConn().prepareStatement(query);
+            conn = dataSource.getConnection();
+            String query = "insert into users (first_name, last_name, username, password, enabled, role_id) values (?,?,?,?,?,?)";
             int i=0;
             stmt.setString(++i,user.getFirstName());
             stmt.setString(++i,user.getLastName());
             stmt.setString(++i,user.getUsername());
             stmt.setString(++i,user.getPassword());
-            stmt.setInt(++i,user.isEnabled());
+            stmt.setBoolean(++i,user.isEnabled());
             stmt.setInt(++i,user.getRole_id());
-            stmt.setDate(++i,(java.sql.Date)user.getDateCreated());
+//            stmt.setDate(++i,(java.sql.Date)user.getDateCreated());
+            stmt = conn.prepareStatement(query);
             stmt.execute();
-
         }catch (SQLException se){
             se.printStackTrace();
         }finally {
-            dbUtil.closeConn();
-            dbUtil.closeStmt(stmt);
+            try{
+                conn.close();
+                stmt.close();
+            }catch (SQLException se){
+                se.printStackTrace();
+            }
         }
     }
 
     @Override
     public List getAll() {
 
+        Connection conn = null;
         PreparedStatement stmt = null;
         ResultSet rs = null;
         List users = new ArrayList();
         User user;
-        Connection conn = null;
+
         try{
             conn = dataSource.getConnection();
             String query = "select * from users;";
@@ -70,22 +75,21 @@ public class UserDAOImpl implements UserDAO {
                 user.setLastName(rs.getString("last_name"));
                 user.setUsername(rs.getString("username"));
                 user.setPassword(rs.getString("password"));
-                user.setEnabled(rs.getInt("enabled"));
+                user.setEnabled(rs.getBoolean("enabled"));
                 user.setRole_id(rs.getInt("role_id"));
-//                this.addChildEntities(user);
+                this.addChildEntities(user);
                 users.add(user);
             }
         }catch (SQLException se){
             se.printStackTrace();
         }finally {
-//            dbUtil.closeConn();
             try {
                 conn.close();
+                stmt.close();
+                rs.close();
             }catch (SQLException se){
-
+                se.printStackTrace();
             }
-            dbUtil.closeStmt(stmt);
-            dbUtil.closeRS(rs);
         }
 
         return users;
@@ -94,15 +98,16 @@ public class UserDAOImpl implements UserDAO {
     @Override
     public Object getById(Integer id) {
 
+        Connection conn = null;
         PreparedStatement stmt = null;
         ResultSet rs = null;
         User user = new User();
 
         try{
-
+            conn = dataSource.getConnection();
             String query = "select * from users where id = ?";
             stmt.setInt(1,id);
-            stmt = dbUtil.getConn().prepareStatement(query);
+            stmt = conn.prepareStatement(query);
             rs = stmt.executeQuery();
 
             while(rs!=null && rs.next()){
@@ -111,16 +116,20 @@ public class UserDAOImpl implements UserDAO {
                 user.setLastName(rs.getString("last_name"));
                 user.setUsername(rs.getString("username"));
                 user.setPassword(rs.getString("password"));
-                user.setEnabled(rs.getInt("enabled"));
+                user.setEnabled(rs.getBoolean("enabled"));
                 user.setRole_id(rs.getInt("role_id"));
                 this.addChildEntities(user);
             }
         }catch (SQLException se){
             se.printStackTrace();
         }finally {
-            dbUtil.closeConn();
-            dbUtil.closeStmt(stmt);
-            dbUtil.closeRS(rs);
+            try {
+                conn.close();
+                stmt.close();
+                rs.close();
+            }catch (SQLException se){
+                se.printStackTrace();
+            }
         }
 
         return user;
@@ -129,23 +138,52 @@ public class UserDAOImpl implements UserDAO {
     @Override
     public void update(Object object) {
 
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        User user = (User)object;
+
+        try{
+            conn = dataSource.getConnection();
+            String query = "update users set first_name=?, last_name=?, username=?, password=?, enabled=?, role_id=? where id=?";
+            int i=0;
+            stmt.setString(++i,user.getFirstName());
+            stmt.setString(++i,user.getLastName());
+            stmt.setString(++i,user.getUsername());
+            stmt.setString(++i,user.getPassword());
+            stmt.setBoolean(++i,user.isEnabled());
+            stmt.setInt(++i,user.getRole_id());
+//            stmt.setDate(++i,(java.sql.Date)user.getDateCreated());
+            stmt = conn.prepareStatement(query);
+            stmt.executeUpdate();
+        }catch (SQLException se){
+            se.printStackTrace();
+        }finally {
+
+        }
     }
 
     @Override
     public void delete(Object object) {
 
+        Connection conn = null;
         PreparedStatement stmt = null;
         User user = (User)object;
+
         try{
+            conn = dataSource.getConnection();
             String query = "delete from users where id = ?";
             stmt.setInt(1, user.getId());
-            stmt = dbUtil.getConn().prepareStatement(query);
+            stmt = conn.prepareStatement(query);
             stmt.execute();
         }catch (Exception ex){
             ex.printStackTrace();
         }finally {
-            dbUtil.closeConn();
-            dbUtil.closeStmt(stmt);
+            try {
+                conn.close();
+                stmt.close();
+            }catch (SQLException se){
+                se.printStackTrace();
+            }
         }
     }
 
@@ -154,13 +192,13 @@ public class UserDAOImpl implements UserDAO {
         user.setAddress(addressDAO.getByUserId(user.getId()));
     }
 
-    @Override
-    public User getUserByUandP(String username, String password) {
-        return null;
-    }
-
-    @Override
-    public User getUserByHash(String hash) {
-        return null;
-    }
+//    @Override
+//    public User getUserByUandP(String username, String password) {
+//        return null;
+//    }
+//
+//    @Override
+//    public User getUserByHash(String hash) {
+//        return null;
+//    }
 }
